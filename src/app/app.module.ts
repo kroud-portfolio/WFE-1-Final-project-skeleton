9bc3fbe42c8b0000c25528ce18c56c0d457ec9e9
Nikos Balaskas <kroud2002@yahoo.gr>
2018-10-01 00:51:26 +0300

    Code commit - upload issue fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { CurrencyComponent } from './components/currency/currency.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WrapperComponent
+    WrapperComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule,
